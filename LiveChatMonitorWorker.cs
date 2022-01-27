b292ee048e0b92db80b59f478fc5a8e8bd018e2d
陳鈞 <jim60105@gmail.com>
2022-01-28 01:28:22 +0800

    重構Monitor Worker，大幅調整邏輯

--- LiveChatMonitorWorker.cs
+++ LiveChatMonitorWorker.cs
@@ -1,70 +1,80 @@
 using Discord;
 using Discord.Webhook;
-using YoutubeLiveChatToDiscord.Models;
 using Newtonsoft.Json;
+using YoutubeLiveChatToDiscord.Models;
 
 namespace YoutubeLiveChatToDiscord
 {
     public class LiveChatMonitorWorker : BackgroundService
     {
         private readonly ILogger<LiveChatMonitorWorker> logger;
-        private readonly string id = Environment.GetEnvironmentVariable("VIDEOID") ?? "";
+        private readonly string id;
         private readonly DiscordWebhookClient client;
+        private readonly FileInfo liveChatFileInfo;
         private long position = 0;
 
         public LiveChatMonitorWorker(ILogger<LiveChatMonitorWorker> _logger,
                                      DiscordWebhookClient _client)
-        => (logger, client) = (_logger, _client);
+        {
+            (logger, client) = (_logger, _client);
+            client.Log += Client_Log;
+
+            id = Environment.GetEnvironmentVariable("VIDEOID") ?? "";
+            if (string.IsNullOrEmpty(id)) throw new ArgumentException(nameof(id));
+
+            liveChatFileInfo = new($"{id}.live_chat.json");
+        }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            while (!liveChatFileInfo.Exists && !stoppingToken.IsCancellationRequested)
+            {
+                logger.LogInformation("{jsonFile} not found.", liveChatFileInfo.FullName);
+                logger.LogInformation($"Wait for {nameof(LiveChatDownloadWorker)} to start.");
+                await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+                liveChatFileInfo.Refresh();
+            }
+            logger.LogInformation("Start Monitoring!");
+
+            using StreamReader sr = new(liveChatFileInfo.OpenRead());
+            position = sr.BaseStream.Length;
+            logger.LogDebug("Start at position: {position}", position);
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await RunAsync();
-#if DEBUG
-                await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
-#else
-                await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
-#endif
-            }
-        }
-
-        public async Task RunAsync()
-        {
-            FileInfo fi = new($"{id}.live_chat.json");
-            if (fi.Exists)
-            {
-                using StreamReader sr = new(fi.OpenRead());
-                if (sr.BaseStream.Length >= position)
+                if (sr.BaseStream.Length <= position)
                 {
-                    sr.BaseStream.Seek(position, SeekOrigin.Begin);
+                    position = sr.BaseStream.Length;
+                    logger.LogTrace("No new chat. Wait 10 sec");
+                    // 每10秒檢查一次json檔
+                    await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+                    continue;
                 }
 
-                while (sr.Peek() >= 0)
+                sr.BaseStream.Seek(position, SeekOrigin.Begin);
+                while (position < sr.BaseStream.Length)
                 {
                     try
                     {
-                        Chat? chat = JsonConvert.DeserializeObject<Chat>(await sr.ReadLineAsync() ?? "");
+                        string? str = await sr.ReadLineAsync();
+                        if (string.IsNullOrEmpty(str)) continue;
 
-                        if (null != chat)
-                        {
-                            logger.LogTrace("Get chat {chat}", chat);
-                            try
-                            {
-                                await SendToDiscordAsync(client, chat);
-                            }
-                            catch (TimeoutException) { }
-                        }
+                        Chat? chat = JsonConvert.DeserializeObject<Chat>(str);
+                        position = sr.BaseStream.Position;
+                        if (null == chat) continue;
+
+                        await BuildRequestAndSendToDiscord(chat, stoppingToken);
                     }
-                    catch (JsonSerializationException) { }
-
-                    position = sr.BaseStream.Position;
+                    catch (JsonSerializationException e)
+                    {
+                        logger.LogError("{error}", e);
+                        position = sr.BaseStream.Position;
+                    }
                 }
             }
         }
 
-        private Task SendToDiscordAsync(DiscordWebhookClient client, Chat chat)
+        private async Task BuildRequestAndSendToDiscord(Chat chat, CancellationToken stoppingToken)
         {
             LiveChatTextMessageRenderer? lctmr = chat.replayChatItemAction?.actions?.FirstOrDefault()?.addChatItemAction?.item?.liveChatTextMessageRenderer;
             List<Run>? runs = lctmr?.message?.runs;
@@ -78,13 +88,54 @@ namespace YoutubeLiveChatToDiscord
                 //eb.WithAuthor(author, authorPhoto);
                 eb.WithTitle("[點此前往影片]");
                 eb.WithUrl($"https://youtu.be/{id}");
-                eb.WithFooter(new EmbedFooterBuilder().WithText(id));
+                eb.WithFooter(new EmbedFooterBuilder().WithText($"{id}, {lctmr?.timestampUsec}"));
 
-                return client.SendMessageAsync(embeds: new Embed[] { eb.Build() },
-                                               username: author,
-                                               avatarUrl: authorPhoto);
+                try
+                {
+                    logger.LogDebug("Sending Request to Discord: {author}, {message}", author, eb.Description);
+                    ulong messageId = await client.SendMessageAsync(embeds: new Embed[] { eb.Build() },
+                                                                username: author,
+                                                                avatarUrl: authorPhoto);
+                    logger.LogDebug("Message sent to discord, message id: {messageId}", messageId);
+
+                    // The rate for Discord webhooks are 30 requests/minute per channel.
+                    // Be careful when you run multiple instances in the same channel!
+                    logger.LogTrace("Wait 2 seconds for discord webhook rate limit");
+                    await Task.Delay(TimeSpan.FromSeconds(2), stoppingToken);
+                }
+                catch (TimeoutException) { }
             }
-            return Task.CompletedTask;
         }
+
+        private Task Client_Log(LogMessage arg)
+        {
+            return Task.Run(() =>
+            {
+                switch (arg.Severity)
+                {
+                    case LogSeverity.Critical:
+                        logger.LogCritical("{message}", arg);
+                        break;
+                    case LogSeverity.Error:
+                        logger.LogError("{message}", arg);
+                        break;
+                    case LogSeverity.Warning:
+                        logger.LogWarning("{message}", arg);
+                        break;
+                    case LogSeverity.Info:
+                        logger.LogInformation("{message}", arg);
+                        break;
+                    case LogSeverity.Verbose:
+                        logger.LogTrace("{message}", arg);
+                        break;
+                    case LogSeverity.Debug:
+                        logger.LogDebug("{message}", arg);
+                        break;
+                    default:
+                        break;
+                }
+            });
+        }
+
     }
 }
