b5800ca60d6c96e7da079e7b556aded28be66a30
陳鈞 <jim60105@gmail.com>
2022-01-28 01:27:33 +0800

    動態尋找yt-dlp，因為在我Win本機和Docker中的路徑不相同

--- LiveChatDownloadWorker.cs
+++ LiveChatDownloadWorker.cs
@@ -6,10 +6,19 @@ namespace YoutubeLiveChatToDiscord;
 public class LiveChatDownloadWorker : BackgroundService
 {
     private readonly ILogger<LiveChatDownloadWorker> logger;
-    private readonly string id = Environment.GetEnvironmentVariable("VIDEOID") ?? "";
-    public static string YtdlPath { get; set; } = "/usr/bin/yt-dlp";
+    private readonly string id;
+    public string YtdlPath;
 
-    public LiveChatDownloadWorker(ILogger<LiveChatDownloadWorker> _logger) => logger = _logger;
+    public LiveChatDownloadWorker(ILogger<LiveChatDownloadWorker> _logger)
+    {
+        logger = _logger;
+
+        id = Environment.GetEnvironmentVariable("VIDEOID") ?? "";
+        if (string.IsNullOrEmpty(id)) throw new ArgumentException(nameof(id));
+
+        YtdlPath = WhereIsYt_dlp();
+        logger.LogDebug("Found yt-dlp at {path}", YtdlPath);
+    }
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
@@ -25,11 +34,25 @@ public class LiveChatDownloadWorker : BackgroundService
         optionSet.AddCustomOption("--ignore-no-formats-error", "");
 
         YoutubeDLProcess ytdlProc = new(YtdlPath);
-        //ytdlProc.OutputReceived += (o, e) => logger.Verbose(e.Data);
-        ytdlProc.ErrorReceived += (o, e) => logger.LogError(e.Data);
+        ytdlProc.OutputReceived += (o, e) => logger.LogTrace("{message}", e.Data);
+        ytdlProc.ErrorReceived += (o, e) => logger.LogError("{error}", e.Data);
 
+        logger.LogInformation("Start yt-dlp!");
         return ytdlProc.RunAsync(new string[] { id },
                                  optionSet,
                                  stoppingToken);
+    }
+
+    public static string WhereIsYt_dlp()
+    {
+        // https://stackoverflow.com/a/63021455
+        string file = "yt-dlp";
+        string[] paths = Environment.GetEnvironmentVariable("PATH")?.Split(';') ?? Array.Empty<string>();
+        string[] extensions = Environment.GetEnvironmentVariable("PATHEXT")?.Split(';') ?? Array.Empty<string>();
+        return (from p in new[] { Environment.CurrentDirectory }.Concat(paths)
+                from e in extensions
+                let path = Path.Combine(p.Trim(), file + e.ToLower())
+                where File.Exists(path)
+                select path)?.FirstOrDefault() ?? "/usr/bin/yt-dlp";
     }
 }
